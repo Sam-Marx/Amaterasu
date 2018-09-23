bf79848f02d1818b27c81cbeb67e87740c6666c7
Sam <27170293+PyOtho@users.noreply.github.com>
2018-09-23 17:22:21 -0300

    Amaterasu - v0.0.1 - 4 modules done.
    PROJECT Amaterasu - v0.0.1 - 4 modules done.
    Penetration tester framework pre-alpha
    Version: 0.0.1;
    Modules done: four (4).

---
+++ amaterasu.py
@@ -0,0 +1,32 @@
+#coding: utf-8
+#!/usr/bin/python3
+
+from core.banner import banner
+from mind.main import main, clear
+from time import *
+from huepy import *
+
+versao = '0.0.1'
+
+def show_info():
+	import os
+	import platform
+
+	name = os.name
+	os = platform.system()
+	system = platform.release()
+
+	print(bold(green('[+] ')) + 'Operating System detected: ' + bold(red(name)) + ', ' + bold(red(os)) + ' ' + bold(red(system)) + '.')
+	print(bold(green('\n 		Welcome to AMATERASU.		\n')))
+
+if __name__ == '__main__':
+	try:
+		clear()
+		banner(versao)
+		sleep(0.5)
+		show_info()
+		while True:
+			main()
+
+	except Exception as e:
+		print(bad('Exception: {}'.format(e)))
