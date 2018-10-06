1b4a9708a1ffb14aecd0d80055e4fd95b742c37e
Sam <27170293+PyOtho@users.noreply.github.com>
2018-10-06 14:14:41 -0300

    Packages updated.

--- mind/modules/modules.py
+++ mind/modules/modules.py
@@ -1,13 +1,32 @@
 #coding: utf-8
 #!/usr/bin/python3
 
-import requests
-import json
+from xml.etree import ElementTree as etree
+from mp3_tagger import MP3File, VERSION_2
+from PIL.ExifTags import TAGS, GPSTAGS
+from PyPDF2 import PdfFileReader
+from bs4 import BeautifulSoup
+from datetime import datetime
 from ipwhois import IPWhois
+from mutagen.mp3 import MP3
+from pprint import pprint
+from ftplib import FTP
+from PIL import Image
 from huepy import *
-import re
-import socket
+import dns.resolver
 import tldextract
+import dns.query
+import requests
+import dns.zone
+import os.path
+import zipfile
+import shutil
+import socket
+import pefile
+import json
+import time
+import os
+import re
 
 def iploc():
 	target = input('Enter domain: ')
