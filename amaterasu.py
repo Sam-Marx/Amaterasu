e53b10094b2be5c741baadaea1b8520c96123a57
Sam <44714584+SamCEAP@users.noreply.github.com>
2018-11-11 18:55:12 -0200

    Update amaterasu.py

--- amaterasu.py
+++ amaterasu.py
@@ -1,12 +1,12 @@
 #coding: utf-8
 #!/usr/bin/python3
 
-from core.banner import banner
+from core.banner import show_banners
 from mind.main import main, clear
 from time import *
 from huepy import *
 
-versao = '0.0.4'
+versao = '0.2.1'
 
 def show_info():
 	import os
@@ -17,13 +17,18 @@ def show_info():
 	system = platform.release()
 
 	print(bold(green('[+] ')) + 'Operating System detected: ' + bold(red(name)) + ', ' + bold(red(os)) + ' ' + bold(red(system)) + '.')
-	print(bold(green('\n 		Welcome to AMATERASU.		\n')))
+	print(bold(green('\t\tWelcome to AMATERASU.')))
 
 if __name__ == '__main__':
 	try:
 		clear()
-		banner(versao)
-		sleep(0.5)
+		show_banners(versao)
+		print('''
+		{}
+		{}
+		{}
+		'''.format(bold(red('AMATERASU')), bold(purple('PENETRATION TESTING FRAMEWORK')), 'v' + versao))
+		sleep(0.4)
 		show_info()
 		while True:
 			main()
