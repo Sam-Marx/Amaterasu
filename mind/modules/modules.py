f00f8fec1b75d974cc6972dbcc1f9bb285c272fa
Sam <44714584+SamCEAP@users.noreply.github.com>
2018-12-06 16:01:54 -0200

    Update modules.py

--- mind/modules/modules.py
+++ mind/modules/modules.py
@@ -149,49 +149,50 @@ def whois():
 				whasnreg = res['asn_registry']
 
 				print()
+				print(bold(green('Domain: ')) + domain)
 				if whname == None:
-					print(bold(red("NAME ERROR: " )) + "Amaterasu can't find the name.")
+					pass
 				else:
 					print(bold(green('Name: ' )) + whname)
 				print(bold(green('IP: ')) + addr)
 				if whdesc == None:
-					print(bold(red("DESCRIPTION ERROR: ")) + "Amaterasu can't find the description.")
+					pass
 				else:
 					print(bold(green('Description: ')) + whdesc)
 				if whcount == None:
-					print(bold(red("Country ERROR: ")) + "Amaterasu can't find the country.")
+					pass
 				else:
 					print(bold(green("Country: ")) + whcount)
 				if whstate == None:
-					print(bold(red("STATE ERROR: ")) + "Amaterasu can't find the state.")
+					pass
 				else:
 					print(bold(green('State: ')) + whstate)
 				if whcity == None:
-					print(bold(red("CITY ERROR: ")) + "Amaterasu can't find the city.")
+					pass
 				else:
 					print(bold(green('City: ')) + whcity)
 				if whadd == None:
-					print(bold(red("ADDRESS ERROR: ")) + "Amaterasu can't find the address.")
+					pass
 				else:
 					print(bold(green('Address: ')) + whadd)
 				if whemail == None:
-					print(bold(red("ABUSE E-MAIL ERROR: " )) + "Amaterasu can't find the abuse e-mail.")
+					pass
 				else:
 					print(bold(green('Abuse e-mail: ')) + whemail)
 				if whcidr == None:
-					print(bold(red("CIDR ERROR: ")) + "Amaterasu can't find the CIDR.")
+					pass
 				else:
 					print(bold(green('CIDR: ')) + whcidr)
 				if whasncidr == None:
-					print(bold(red("ASN CIDR ERROR: ")) + "Amaterasu can't find the ASN_CIDR.")
+					pass
 				else:
 					print(bold(green('ASN CIDR: ')) + whasncidr)
 				if whasn == None:
-					print(bold(red("ASN ERROR: ")) + "Amaterasu can't find the ASN.")
+					pass
 				else:
 					print(bold(green('ASN: ')) + whasn)
 
-		if target.startswith('http://'):
+		elif target.startswith('http://') or target.startswith('https://'):
 			ext = tldextract.extract(target)
 			domain = ext.domain
 			suffix = ext.suffix
@@ -217,109 +218,43 @@ def whois():
 
 			print()
 			if whname == None:
-				print(bold(red("NAME ERROR: " )) + "Amaterasu can't find the name.")
+				pass
 			else:
 				print(bold(green('Name: ' )) + whname)
 			if whdesc == None:
-				print(bold(red("DESCRIPTION ERROR: ")) + "Amaterasu can't find the description.")
+				pass
 			else:
 				print(bold(green('Description: ')) + whdesc)
 			if whcount == None:
-				print(bold(red("Country ERROR: ")) + "Amaterasu can't find the country.")
+				pass
 			else:
 				print(bold(green("Country: ")) + whcount)
 			if whstate == None:
-				print(bold(red("STATE ERROR: ")) + "Amaterasu can't find the state.")
+				pass
 			else:
 				print(bold(green('State: ')) + whstate)
 			if whcity == None:
-				print(bold(red("CITY ERROR: ")) + "Amaterasu can't find the city.")
+				pass
 			else:
 				print(bold(green('City: ')) + whcity)
 			if whadd == None:
-				print(bold(red("ADDRESS ERROR: ")) + "Amaterasu can't find the address.")
+				pass
 			else:
 				print(bold(green('Address: ')) + whadd)
 			if whemail == None:
-				print(bold(red("ABUSE E-MAIL ERROR: " )) + "Amaterasu can't find the abuse e-mail.")
+				pass
 			else:
 				print(bold(green('Abuse e-mail: ')) + whemail)
 			if whcidr == None:
-				print(bold(red("CIDR ERROR: ")) + "Amaterasu can't find the CIDR.")
+				pass
 			else:
 				print(bold(green('CIDR: ')) + whcidr)
 			if whasncidr == None:
-				print(bold(red("ASN CIDR ERROR: ")) + "Amaterasu can't find the ASN_CIDR.")
+				pass
 			else:
 				print(bold(green('ASN CIDR: ')) + whasncidr)
 			if whasn == None:
-				print(bold(red("ASN ERROR: ")) + "Amaterasu can't find the ASN.")
-			else:
-				print(bold(green('ASN: ')) + whasn)
-
-		elif target.startswith('https://'):
-			ext = tldextract.extract(target)
-			domain = ext.domain
-			suffix = ext.suffix
-
-			fullsite = domain + '.' + suffix
-
-			addr = socket.gethostbyname(fullsite)
-			obj = IPWhois(addr)
-			res = obj.lookup()
-
-			whname = res["nets"][0]['name']
-			whdesc = res["nets"][0]['description']
-			whemail = res["nets"][0]['abuse_emails']
-			whcount = res["nets"][0]['country']
-			whstate = res["nets"][0]['state']
-			whcidr = res["nets"][0]['cidr']
-			whcity = res["nets"][0]['city']
-			whadd = res["nets"][0]['address']
-			whasncidr = res['asn_cidr']
-			whasn = res['asn']
-			whasndt = res['asn_date']
-			whasnreg = res['asn_registry']
-
-			print()
-			if whname == None:
-				print(bold(red("NAME ERROR: " )) + "Amaterasu can't find the name.")
-			else:
-				print(bold(green('Name: ' )) + whname)
-			if whdesc == None:
-				print(bold(red("DESCRIPTION ERROR: ")) + "Amaterasu can't find the description.")
-			else:
-				print(bold(green('Description: ')) + whdesc)
-			if whcount == None:
-				print(bold(red("Country ERROR: ")) + "Amaterasu can't find the country.")
-			else:
-				print(bold(green("Country: ")) + whcount)
-			if whstate == None:
-				print(bold(red("STATE ERROR: ")) + "Amaterasu can't find the state.")
-			else:
-				print(bold(green('State: ')) + whstate)
-			if whcity == None:
-				print(bold(red("CITY ERROR: ")) + "Amaterasu can't find the city.")
-			else:
-				print(bold(green('City: ')) + whcity)
-			if whadd == None:
-				print(bold(red("ADDRESS ERROR: ")) + "Amaterasu can't find the address.")
-			else:
-				print(bold(green('Address: ')) + whadd)
-			if whemail == None:
-				print(bold(red("ABUSE E-MAIL ERROR: " )) + "Amaterasu can't find the abuse e-mail.")
-			else:
-				print(bold(green('Abuse e-mail: ')) + whemail)
-			if whcidr == None:
-				print(bold(red("CIDR ERROR: ")) + "Amaterasu can't find the CIDR.")
-			else:
-				print(bold(green('CIDR: ')) + whcidr)
-			if whasncidr == None:
-				print(bold(red("ASN CIDR ERROR: ")) + "Amaterasu can't find the ASN_CIDR.")
-			else:
-				print(bold(green('ASN CIDR: ')) + whasncidr)
-			if whasn == None:
-				print(bold(red("ASN ERROR: ")) + "Amaterasu can't find the ASN.")
+				pass
 			else:
 				print(bold(green('ASN: ')) + whasn)
 		else:
@@ -342,43 +277,43 @@ def whois():
 
 			print()
 			if whname == None:
-				print(bold(red("NAME ERROR: " )) + "Amaterasu can't find the name.")
+				pass
 			else:
 				print(bold(green('Name: ' )) + whname)
 			if whdesc == None:
-				print(bold(red("DESCRIPTION ERROR: ")) + "Amaterasu can't find the description.")
+				pass
 			else:
 				print(bold(green('Description: ')) + whdesc)
 			if whcount == None:
-				print(bold(red("Country ERROR: ")) + "Amaterasu can't find the country.")
+				pass
 			else:
 				print(bold(green("Country: ")) + whcount)
 			if whstate == None:
-				print(bold(red("STATE ERROR: ")) + "Amaterasu can't find the state.")
+				pass
 			else:
 				print(bold(green('State: ')) + whstate)
 			if whcity == None:
-				print(bold(red("CITY ERROR: ")) + "Amaterasu can't find the city.")
+				pass
 			else:
 				print(bold(green('City: ')) + whcity)
 			if whadd == None:
-				print(bold(red("ADDRESS ERROR: ")) + "Amaterasu can't find the address.")
+				pass
 			else:
 				print(bold(green('Address: ')) + whadd)
 			if whemail == None:
-				print(bold(red("ABUSE E-MAIL ERROR: " )) + "Amaterasu can't find the abuse e-mail.")
+				pass
 			else:
 				print(bold(green('Abuse e-mail: ')) + whemail)
 			if whcidr == None:
-				print(bold(red("CIDR ERROR: ")) + "Amaterasu can't find the CIDR.")
+				pass
 			else:
 				print(bold(green('CIDR: ')) + whcidr)
 			if whasncidr == None:
-				print(bold(red("ASN CIDR ERROR: ")) + "Amaterasu can't find the ASN_CIDR.")
+				pass
 			else:
 				print(bold(green('ASN CIDR: ')) + whasncidr)
 			if whasn == None:
-				print(bold(red("ASN ERROR: ")) + "Amaterasu can't find the ASN.")
+				pass
 			else:
 				print(bold(green('ASN: ')) + whasn)
 	except Exception:
