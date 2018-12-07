d526c51cf61e2518f45733ccafa653b040757768
Sam <44714584+SamCEAP@users.noreply.github.com>
2018-12-07 19:29:32 -0200

    Login panel finder added

--- mind/modules/modules.py
+++ mind/modules/modules.py
@@ -886,3 +886,57 @@ def metadata():
 		print()
 		#print('Soon.')
 		#target = input(strike(que('Enter target: ')))
+
+def findPanel():
+	try:
+		target = input('Enter domain: ')
+		file = input('Enter panel list: ')
+
+		ext = tldextract.extract(target)
+		domain = ext.domain
+		suffix = ext.suffix
+		panelsFound = []
+		print()
+		if target.startswith('http://') or target.startswith('https://'):
+			try:
+				requests.get(target)
+			except Exception as e:
+				print(bad('Error: ' + str(e)))
+
+			r = requests.get(target)
+			f = open(file)
+			for a in f.readlines():
+				a = a.strip()
+				r = requests.get(target + '/' + a)
+
+				if r.status_code == 200 or r.status_code == 301:
+					print(bold(good('Login panel found: ' + target + '/' + a)))
+					panelsFound.append(target + '/' + a)
+
+				elif r.status_code == 404:
+					print(bold(bad('Login panel not found: ' + target + '/' + a)))
+
+		else:
+			target = 'http://' + domain + '.' + suffix
+			try:
+				requests.get(target)
+			except Exception as e:
+				print(bad('Error: ' + str(e)))
+
+			r = requests.get(target)
+			f = open(file)
+			for a in f.readlines():
+				a = a.strip()
+				r = requests.get(target + '/' + a)
+
+				if r.status_code == 200 or r.status_code == 301:
+					print(bold(good('Login panel found: ' + target + '/' + a)))
+					panelsFound.append(target + '/' + a)
+
+				elif r.status_code == 404:
+					print(bold(bad('Login panel not found: ' + target + '/' + a)))
+	except KeyboardInterrupt:
+		print()
+		for p in panelsFound:
+			print(bold(good('Panel: ')) + p)
+		print(bold(good('Found: ' + str(len(panelsFound)))))
