90ea0c77169aadaa2624659061db7b20adbfc7dc
Sam <44714584+SamCEAP@users.noreply.github.com>
2018-12-13 23:04:15 -0200

    Update main.py

--- mind/main.py
+++ mind/main.py
@@ -3,15 +3,8 @@
 
 #MAIN
 from core.main_imports import *
-
-#update
-def update():
-	if platform.system() == 'Windows':
-		print(bold(bad('Amaterasu cant be updated in Windows OS.')))
-	else:
-		os.system('git clone --depth=1 https://github.com/SamCEAP/Amaterasu.git')
-		os.system('cd Amaterasu')
-		os.system('python3 amaterasu.py')
+from core.main_update import *
+from core.main_clear import *
 
 def main():
 	print_help()
@@ -68,12 +61,6 @@ def main():
 						except KeyboardInterrupt:
 							print()
 							main()
-					elif user.split(' ')[1] == 'social':
-						try:
-							social()
-						except KeyboardInterrupt:
-							print()
-							main()
 					elif user.split(' ')[1] == 'dns_ex':
 						try:
 							dns_ex()
@@ -102,6 +89,47 @@ def main():
 					show_module()
 					print(bold(info('Select a module.')))
 
+			elif user.startswith('set'):
+				try:
+					if user.split(' ')[1] == 'shodan_api':
+						with open('core/keys.json', 'r+') as f:
+							apiKeys = json.load(f)
+							tmp = apiKeys
+							apiKeys['APIs']['SHODAN'] = user.split(' ')[2]
+							apiKeys['APIs']['SHODAN_CHECK'] = "True"
+							f.seek(0)
+							json.dump(apiKeys, f, indent=4)
+							print(bold(info('Shodan API\t' + user.split(' ')[2])))
+						f.close()
+
+					elif user.split(' ')[1] == 'censys_uid':
+						with open('core/keys.json', 'r+') as f:
+							apiKeys = json.load(f)
+							tmp = apiKeys
+							apiKeys['APIs']['CENSYS_UID'] = user.split(' ')[2]
+							apiKeys['APIs']['CENSYS_UID_CHECK'] = "True"
+							f.seek(0)
+							json.dump(apiKeys, f, indent=4)
+							print(bold(info('Censys UID\t\t' + user.split(' ')[2])))
+							f.close()
+					elif user.split(' ')[1] == 'censys_secret':
+						with open('core/keys.json', 'r+') as f:
+							apiKeys = json.load(f)
+							tmp = apiKeys
+							apiKeys['APIs']['CENSYS_SECRET'] = user.split(' ')[2]
+							apiKeys['APIs']['CENSYS_SECRET_CHECK'] = "True"
+							f.seek(0)
+							json.dump(apiKeys, f, indent=4)
+							print(bold(info('Censys SECRET\t' + user.split(' ')[2])))
+							f.close()
+
+				except Exception as e:
+					print(bold(bad('Error: {}'.format(str(e)))))
+					main()
+				except KeyboardInterrupt:
+					print()
+					main()
+
 			elif user.startswith('show'):
 				try:
 					if user.split(' ')[1] == 'modules':
@@ -109,11 +137,13 @@ def main():
 					elif user.split(' ')[1] == 'author':
 						author()
 					elif user.split(' ')[1] == 'banners':
-						show_banners()
+						show_banners(versao)
 					elif user.split(' ')[1] == 'help':
 						show_help()
 					elif user.split(' ')[1] == 'bm':
 						aboutme()
+					elif user.split(' ')[1] == 'keys':
+						show_keys()
 				except IndexError:
 					print(bold(info('Select what to show.\n')))
 					print(bold(info('Modules\t\tshow modules')))
@@ -123,11 +153,14 @@ def main():
 					print(bold(info('Help\t\tshow help')))
 
 			elif user == 'exit':
+				print(bold(good('Thanks for using Amaterasu.')))
 				sys.exit()
-			elif user == 'update':
-				update()
 			elif user == 'cls' or user == 'clear':
 				clear()
+			elif user == 'update':
+				update()
+			elif user == '?':
+				show_help()
 			else:
 				print(bad('Amaterasu could not understand.'))
 	except KeyboardInterrupt:
