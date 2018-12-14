afc87c65d37a78e6c35b371a9ff59851991dff39
Sam <44714584+SamCEAP@users.noreply.github.com>
2018-12-13 22:56:44 -0200

    Update main_imports.py

--- core/main_imports.py
+++ core/main_imports.py
@@ -1,12 +1,28 @@
 #coding: utf-8
 #!/usr/bin/python3
-#main imports
 
+#main imports
 from core.banner import show_banners
 from mind.modules.modules import *
 from mind.modules.bm import *
+from core.Config.MAIN_CONFIG import DIR
 from core.main_functions import *
 
+#normal imports
 from huepy import *
 import platform
 import sys
+
+#modules imports
+from mind.modules.dns_extractor import *
+from mind.modules.iplocator import *
+from mind.modules.login_panel_finder import *
+from mind.modules.metadata_extractor import *
+from mind.modules.reverse_ip import *
+from mind.modules.spider import *
+from mind.modules.ssh_bruteforce import *
+from mind.modules.subdomain_extractor import *
+from mind.modules.whois import *
+from mind.modules.network_mapper import *
+from mind.modules.ftp_bruteforce import *
+from mind.modules.email_extractor import *
