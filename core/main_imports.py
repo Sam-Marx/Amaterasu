31fa91fae2ccffec979869108fc0c85f5de8a611
Sam <44714584+SamCEAP@users.noreply.github.com>
2018-12-13 22:17:09 -0200

    Create main_imports.py

---
+++ core/main_imports.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+#!/usr/bin/python3
+#main imports
+
+from core.banner import show_banners
+from mind.modules.modules import *
+from mind.modules.bm import *
+from core.Config.MAIN_CONFIG import DIR
+from core.main_functions import *
+
+from huepy import *
+import platform
+import sys
