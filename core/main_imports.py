488aa70b9a4172c41ce8a10f8abe30648f18f49c
Sam <44714584+SamCEAP@users.noreply.github.com>
2018-12-26 13:25:39 -0200

    Update main_imports.py

--- core/main_imports.py
+++ core/main_imports.py
@@ -10,9 +10,10 @@ from ruamel.yaml import YAML
 from huepy import *
 import platform
 import sys
+import glob
 
 #modules imports
-from mind.modules.dns_extractor import *
+from mind.modules.dns_bruteforce import *
 from mind.modules.iplocator import *
 from mind.modules.login_panel_finder import *
 from mind.modules.metadata_extractor import *
@@ -20,7 +21,7 @@ from mind.modules.reverse_ip import *
 from mind.modules.spider import *
 from mind.modules.ssh_bruteforce import *
 from mind.modules.subdomain_extractor import *
-from mind.modules.whois import *
+from mind.modules.whois_extractor import *
 from mind.modules.network_mapper import *
 from mind.modules.ftp_bruteforce import *
 from mind.modules.email_extractor import *
