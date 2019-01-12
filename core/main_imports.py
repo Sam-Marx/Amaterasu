4437eea93297b333c5391a265c32cdaabe216331
Sam <44714584+SamCEAP@users.noreply.github.com>
2019-01-12 17:26:40 -0200

    Update main_imports.py

--- core/main_imports.py
+++ core/main_imports.py
@@ -10,17 +10,10 @@ from huepy import *
 import platform
 import sys
 import glob
+import os
+import pathlib
 
 #modules imports
-from mind.modules.dns_bruteforce import *
-from mind.modules.iplocator import *
-from mind.modules.login_panel_finder import *
-from mind.modules.metadata_extractor import *
-from mind.modules.reverse_ip import *
-from mind.modules.spider import *
-from mind.modules.ssh_bruteforce import *
-from mind.modules.subdomain_extractor import *
-from mind.modules.whois_extractor import *
-from mind.modules.network_mapper import *
-from mind.modules.ftp_bruteforce import *
-from mind.modules.email_extractor import *
+from mind.modules.BruteforceImports import * #Importing Bruteforce modules
+from mind.modules.InformationGatheringImports import * #Importing Information Gathering modules
+from mind.modules.NetworkImports import * #Importing Network modules
