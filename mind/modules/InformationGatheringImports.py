aa397e2ee715a6debde68906505ab117ffcc22ab
Sam <44714584+SamCEAP@users.noreply.github.com>
2019-01-12 17:33:37 -0200

    Create InformationGatheringImports.py

---
+++ mind/modules/InformationGatheringImports.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+#!/usr/bin/python3
+
+#main imports
+from core.banner import show_banners
+
+#normal imports
+from ruamel.yaml import YAML
+from huepy import *
+import platform
+import sys
+import glob
+import os
+import pathlib
+
+#InformationGathering modules imports
+from mind.modules.InformationGathering.metadata_extractor import *
+from mind.modules.InformationGathering.spider import *
+from mind.modules.InformationGathering.subdomain_extractor import *
+from mind.modules.InformationGathering.whois_extractor import *
+from mind.modules.InformationGathering.email_extractor import *
+from mind.modules.InformationGathering.num_verifier import *
