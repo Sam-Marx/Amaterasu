6eb5984e65b350cb8d0d7c61053089a635f62f1f
Sam <44714584+SamCEAP@users.noreply.github.com>
2019-01-12 17:27:49 -0200

    Update main_update.py

--- core/main_update.py
+++ core/main_update.py
@@ -6,7 +6,11 @@ from core.main_imports import *
 def update():
 	if platform.system() == 'Windows':
 		import urllib.request
-		urllib.request.urlretrieve('https://github.com/SamCEAP/Amaterasu/archive/v1.3.zip', 'Amaterasu.zip')
+		try:
+			urllib.request.urlretrieve('https://github.com/SamCEAP/Amaterasu/archive/v1.3.zip', 'Amaterasu.zip')
+			print(bold(info('Downloaded Amaterasu as Amaterasu.zip.')))
+		except Exception as e:
+			print(bold(bad('Error: {}'.format(e))))
 	else:
 		os.system('git clone --depth=1 https://github.com/SamCEAP/Amaterasu.git')
 		os.system('cd Amaterasu')
