1de9a3c65cd761a5d080c883be93774d7b6b2c85
Sam <44714584+SamCEAP@users.noreply.github.com>
2019-01-15 16:14:04 -0200

    Update create_necessary.py

--- core/create_necessary.py
+++ core/create_necessary.py
@@ -1,34 +1,18 @@
 #coding: utf-8
 #!/usr/bin/python3
 
-from core.main_imports import *
+import os
+from huepy import *
+import pathlib
 
 def create_necessary():
 	try:
-		if os.path.isdir('E-mails') == True:
+		if os.path.isdir('Results') == True:
 			pass
 		else:
-			p = pathlib.Path('E-mails/')
+			p = pathlib.Path('Results/')
 			p.mkdir(parents=True)
-			print(bold(info('Created ' + bold(lightred('E-mails ')) + 'directory.')))
-		if os.path.isdir('Links') == True:
-			pass
-		else:
-			p = pathlib.Path('Links/')
-			p.mkdir(parents=True)
-			print(bold(info('Created ' + bold(lightred('Links ')) + 'directory.')))
-		if os.path.isdir('Subdomains') == True:
-			pass
-		else:
-			p = pathlib.Path('Subdomains/')
-			p.mkdir(parents=True)
-			print(bold(info('Created ' + bold(lightred('Subdomains ')) + 'directory.')))
-		if os.path.isdir('ReverseIP_Domains') == True:
-			pass
-		else:
-			p = pathlib.Path('ReverseIP_Domains/')
-			p.mkdir(parents=True)
-			print(bold(info('Created ' + bold(lightred('ReverseIP_Domains ')) + 'directory.')))
+			print(bold(info('Created ' + bold(lightred('Results ')) + 'directory.')))
 		print()
 
 	except Exception as e:
