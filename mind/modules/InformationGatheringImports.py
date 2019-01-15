5f3e2394731dd974cd97c5fbfbce28e6aa2ba010
Sam <44714584+SamCEAP@users.noreply.github.com>
2019-01-15 16:40:24 -0200

    Update InformationGatheringImports.py

--- mind/modules/InformationGatheringImports.py
+++ mind/modules/InformationGatheringImports.py
@@ -1,19 +1,6 @@
 #coding: utf-8
 #!/usr/bin/python3
 
-#main imports
-from core.banner import show_banners
-
-#normal imports
-from ruamel.yaml import YAML
-from huepy import *
-import platform
-import sys
-import glob
-import os
-import pathlib
-
-#InformationGathering modules imports
 from mind.modules.InformationGathering.num_verifier import *
 from mind.modules.InformationGathering.honeypotDetector import *
 from mind.modules.InformationGathering.spider import *
