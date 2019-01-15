0239ce474eed4efcebd32fd17fb97bde6de33ea4
Sam <44714584+SamCEAP@users.noreply.github.com>
2019-01-15 17:44:01 -0200

    Update InformationGatheringImports.py

--- mind/modules/InformationGatheringImports.py
+++ mind/modules/InformationGatheringImports.py
@@ -5,6 +5,6 @@ from mind.modules.InformationGathering.num_verifier import *
 from mind.modules.InformationGathering.honeypotDetector import *
 from mind.modules.InformationGathering.spider import *
 from mind.modules.InformationGathering.metadata_extractor import *
-from mind.modules.InformationGathering.subdomain_extractor import *
+#from mind.modules.InformationGathering.subdomain_extractor import *
 from mind.modules.InformationGathering.whois_extractor import *
 from mind.modules.InformationGathering.email_extractor import *
