73a102f4100c11f3be27e5314a2b49581115bac0
Sam <44714584+SamCEAP@users.noreply.github.com>
2019-01-15 16:40:10 -0200

    Update NetworkImports.py

--- mind/modules/NetworkImports.py
+++ mind/modules/NetworkImports.py
@@ -1,19 +1,6 @@
 #coding: utf-8
 #!/usr/bin/python3
 
-#main imports
-from core.banner import show_banners
-
-#normal imports
-from ruamel.yaml import YAML
-from huepy import *
-import platform
-import sys
-import glob
-import os
-import pathlib
-
-#modules imports
 from mind.modules.Network.dns_extractor import *
 from mind.modules.Network.iplocator import *
 from mind.modules.Network.reverse_ip import *
