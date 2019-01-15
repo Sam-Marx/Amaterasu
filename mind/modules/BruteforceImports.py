b4f447dd2972d25964c63d0189aa71c0800a8c2a
Sam <44714584+SamCEAP@users.noreply.github.com>
2019-01-15 16:39:50 -0200

    Update BruteforceImports.py

--- mind/modules/BruteforceImports.py
+++ mind/modules/BruteforceImports.py
@@ -1,20 +1,7 @@
 #coding: utf-8
 #!/usr/bin/python3
 
-#main imports
-from core.banner import show_banners
-
-#normal imports
-from ruamel.yaml import YAML
-from huepy import *
-import platform
-import sys
-import glob
-import os
-import pathlib
-
-#modules imports
 from mind.modules.Bruteforce.login_panel_finder import *
-#from mind.modules.Bruteforce.ssh_bruteforce import *
+from mind.modules.Bruteforce.ssh_bruteforce import *
 from mind.modules.Bruteforce.ftp_bruteforce import *
 from mind.modules.Bruteforce.gmail_bruteforce import *
