85648336c04ec2129a0821b27b830c6dcd385cf8
Sam <44714584+SamCEAP@users.noreply.github.com>
2019-01-15 16:13:41 -0200

    Update main_show.py

--- core/main_show.py
+++ core/main_show.py
@@ -1,57 +1,126 @@
 #coding: utf-8
 #!/usr/bin/python3
-#functions show
 
-from core.main_imports import *
+#from core.main_imports import *
+from ruamel.yaml import YAML
+from huepy import *
 
 #show help
 def show_help():
 	print()
-	print(bold(info(' Help:					print this help message.')))
-	print(bold(info(' Exit:					leave the program.')))
-	print(bold(info(' Use:					use module.')))
-	print(bold(info(' Show modules:				show modules.')))
-	print(bold(info(' Clear:					clear the terminal.')))
-	print(bold(info(' Update:					update the framework.')))
-	print(bold(info(' Author:					about Amaterasu framework.')))
+	sHelp = {'help':'print this help message.',
+	'exit':'exit amaterasu',
+	'use (MODULE)':'use a module',
+	'show (apis|banners)':'show modules or APIs',
+	'clear':'clear terminal',
+	'update':'update amaterasu',
+	'author':'about author of amaterasu'}
 
-#check available modules
-def checkAvailable(folder):
-	q = glob.glob('mind/modules/{}/*.py'.format(folder))
-	return(str(int(len(q)) - 1) + str(' modules'))
-	
+	print(bold('COMMAND\t\t\tDESCRIPTION'))
+	print(bold('-------\t\t\t-----------'))
+	for a, b in sHelp.items():
+		if len(a) > 15:
+			print(bold(a + '\t' + b))
+		elif len(a) <= 6:
+			print(bold(a + '\t\t\t' + b))
+		else:
+			print(bold(a + '\t\t' + b))
+
 #show modules
 def show_module():
-	print()
-	print(bold(cyan('Bruteforcing ')) + bold(purple('| ')) + bold(cyan(checkAvailable('Bruteforce'))))
-	print('	FTP bruteforce 			:		ftp_bruteforce		| Bruteforce FTP')
-	print('	SSH bruteforce 			:		ssh_bruteforce		| Bruteforce SSH')
-	print('	Login panel			:		panelfinder		| Bruteforce dir to find login panels')
-	print('	Gmail bruteforce 		:		gmail_bruteforce	| Bruteforce Gmail account')
+	bruteforceModules = {'ftp_bruteforce':'Bruteforce FTP',
+	'ssh_bruteforce':'Bruteforce SSH',
+	'gmail_bruteforce':'Bruteforce Gmail',
+	'panelfinder':'bruteforce dirs to find login panels'}
 	print()
 
-	print(bold(cyan('Network ')) + bold(purple('| ')) + bold(cyan(checkAvailable('Network'))))
-	print('	IP locator			:		iplocator 		| Get IP location')
-	print('	Reverse IP			:		reverse 		| IP domain lookup')
-	print('	DNS records			:		dns_extractor 			| Extract DNS records')
-	print('	Network Mapper			:		network_mapper		| Map network with NMap')
+	print(bold(green('Bruteforce modules')))
+	print(bold('COMMAND\t\t\tDESCRIPTION'))
+	print(bold('-------\t\t\t-----------'))
+	for a, b in bruteforceModules.items():
+		if len(a) > 15:
+			print(bold(a + '\t' + b))
+		elif len(a) <= 6:
+			print(bold(a + '\t\t\t' + b))
+		else:
+			print(bold(a + '\t\t' + b))
 	print()
 
-	print(bold(cyan('Information gathering ')) + bold(purple('| ')) + bold(cyan(checkAvailable('InformationGathering'))))
-	print('	E-mail extraction		:		email_extractor		| Extract e-mail address')
-	print('	Whois information		:		whois_extractor 	| Get whois information')
-	print('	Metadata extraction		:		metadata_extractor 		| Extract metadata from files')
-	print('	Spidering			:		spider 			| Extract links')
-	print('	Subdomain discovery		:		subdomain 		| Discover subdomain')
-	print('	Honeypot detector		:		honeypot_detector	| Scan IP for honeypot (needs shodan)')
+	###
+
+	networkModules = {'iplocator':'get ip location',
+	'reverse_ip':'ip domain lookup',
+	'dns_extractor':'extract dns records',
+	'network_mapper': 'map network with nmap'}
+	print(bold(green('Network modules')))
+	print(bold('COMMAND\t\t\tDESCRIPTION'))
+	print(bold('-------\t\t\t-----------'))
+	for a, b in networkModules.items():
+		if len(a) > 15:
+			print(bold(a + '\t' + b))
+		elif len(a) <= 6:
+			print(bold(a + '\t\t\t' + b))
+		else:
+			print(bold(a + '\t\t' + b))
+	print()
+	
+	###
+
+	informationGatheringModules = {'email_extractor':'extract e-mail address',
+	'number_extractor': 'extract phone numbers',
+	'number_verify': 'verify number information',
+	'whois_extractor': 'get whois information',
+	'metadata_extractor': 'extract metadata from files',
+	'spider': 'extract links from domains',
+	'subdomain': 'discover subdomains',
+	'mysql_scanner': 'scan website for mysql error based',
+	'honeypot_detector':'scan ip for honeypot (needs shodan)'}
+
+	print(bold(green('Information gathering modules')))
+	print(bold('COMMAND\t\t\tDESCRIPTION'))
+	print(bold('-------\t\t\t-----------'))
+	for a, b in informationGatheringModules.items():
+		if len(a) > 15:
+			print(bold(a + '\t' + b))
+		elif len(a) <= 6:
+			print(bold(a + '\t\t\t' + b))
+		else:
+			print(bold(a + '\t\t' + b))
 	print()
 
-#show author
+	exploitatonModules = {'mysql_exploiter':'exploit website with mysql error based'}
+	print(bold(green('Exploitation modules')))
+	print(bold('COMMAND\t\t\tDESCRIPTION'))
+	print(bold('-------\t\t\t-----------'))
+	for a, b in exploitatonModules.items():
+		if len(a) > 15:
+			print(bold(a + '\t' + b))
+		elif len(a) <= 6:
+			print(bold(a + '\t\t\t' + b))
+		else:
+			print(bold(a + '\t\t' + b))
+	print()
+
+	postExploitationModules = {'msfvenom_backdoor':'use msfvenom for backdoor creation'}
+	print(bold(green('Post exploitation modules')))
+	print(bold('COMMAND\t\t\tDESCRIPTION'))
+	print(bold('-------\t\t\t-----------'))
+	for a, b in postExploitationModules.items():
+		if len(a) > 15:
+			print(bold(a + '\t' + b))
+		elif len(a) <= 6:
+			print(bold(a + '\t\t\t' + b))
+		else:
+			print(bold(a + '\t\t' + b))
+	print()
+
+#author
 def author():
 	print()
 	print(bold(lightred('About the author.')))
-	print('Author: Sam Marx')
-	print('Github: https://github.com/SamCEAP/')
+	print(bold('Author: Sam Marx'))
+	print(bold('Github: https://github.com/SamCEAP/'))
+	print(bold('Twitter: https://twitter.com/Sam_Mrx'))
 
 #show APIs
 def show_API():
