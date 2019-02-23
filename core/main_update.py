8e4585565ef79f201793adf0d0432d74bec9aac4
Sam <sam-marx@protonmail.com>
2019-02-23 20:18:29 -0300

    updated.

--- core/main_update.py
+++ core/main_update.py
@@ -6,18 +6,27 @@ import platform
 import os
 from huepy import *
 
-version = '1.5'
+version = open('core/current_release.txt', 'r')
+version = version.read()
 
 def update():
-	r = requests.get('https://raw.githubusercontent.com/SamCEAP/Amaterasu/master/core/current_release.txt')
+	r = requests.get('https://raw.githubusercontent.com/Sam-Marx/Amaterasu/master/core/current_release.txt')
 	if float(r.text) > float(version):
 		print(bold(info('Current version: ' + version)))
 		print(bold(info('Amaterasu can be updated. New version: ' + r.text)))
 		if platform.system() == 'Windows':
 			print(bold(bad('Amaterasu needs to be updated manually.')))
 		else:
-			os.system('git clone --depth=1 https://github.com/SamCEAP/Amaterasu.git')
-			os.system('cd Amaterasu')
-			os.system('python3 amaterasu.py')
+			if os.getcwd().endswith('Amaterasu'):
+				os.system('cd ..')
+				os.system('rm -rf Amaterasu/')
+				os.system('git clone --depth=1 https://github.com/Sam-Marx/Amaterasu.git')
+				os.system('cd Amaterasu')
+				os.system('python3 amaterasu.py')
+			else:
+				os.system('rm -rf Amaterasu/')
+				os.system('git clone --depth=1 https://github.com/Sam-Marx/Amaterasu.git')
+				os.system('cd Amaterasu')
+				os.system('python3 amaterasu.py')
 	else:
 		print(bold(good('Amaterasu is updated.')))
