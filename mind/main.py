9cc4bde9337c12dc83850b2cadd8e64696188e61
Sam <sam-marx@protonmail.com>
2019-02-22 23:12:51 -0300

    New functions
    Added import verifier and new modules

--- mind/main.py
+++ mind/main.py
@@ -5,6 +5,7 @@
 from mind.modules.BruteforceImports import * #Importing Bruteforce modules
 from mind.modules.InformationGatheringImports import * #Importing Information Gathering modules
 from mind.modules.NetworkImports import * #Importing Network modules
+from mind.modules.Exploitation import * #Importing Exploitation modules
 
 from core.main_update import *
 from core.main_clear import *
@@ -16,100 +17,181 @@ from huepy import *
 import sys
 import os
 
+from core.main_clear import *
+from core.checkErrors import *
+
 def main():
 	show_help()
+	import_file = open('core/import.yaml').read()
+	yaml = YAML()
+	importE = yaml.load(import_file) #import error
+	bruteforce = importE['BRUTEFORCE']
+	informationG = importE['INFORMATION_GATHERING']
+	exploitation = importE['EXPLOITATION']
+	network = importE['NETWORK']
+
 	try:
 		while True:
 			user = input(bold(red('\nAMATERASU ') + '> '))
 			if user.startswith('use'):
 				try:
 					if user.split(' ')[1] == 'network_mapper':
-						try:
-							network_mapper_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if network[0]['network_mapper'].startswith('Pass'):
+							try:
+								network_mapper_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'whois_extractor':
-						try:
-							whois_extractor_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if informationG[7]['whois_extractor'].startswith('Pass'):
+							try:
+								whois_extractor_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'email_extractor':
-						try:
-							email_extractor_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if informationG[0]['email_extractor'].startswith('Pass'):
+							try:
+								email_extractor_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'metadata_extractor':
-						try:
-							metadata_extractor_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if informationG[3]['metadata_extractor'].startswith('Pass'):
+							try:
+								metadata_extractor_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'reverse_ip':
-						try:
-							reverse_ip_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if network[1]['reverse_ip'].startswith('Pass'):
+							try:
+								reverse_ip_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'iplocator':
-						try:
-							ip_locator_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if network[2]['iplocator'].startswith('Pass'):
+							try:
+								ip_locator_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'spider':
-						try:
-							spider_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if informationG[6]['spider'].startswith('Pass'):
+							try:
+								spider_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'dns_extractor':
-						try:
-							dns_extractor_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if network[3]['dns_extractor'].startswith('Pass'):
+							try:
+								dns_extractor_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'ftp_bruteforce':
-						try:
-							ftp_bruteforce_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if bruteforce[0]['ftp_bruteforce'].startswith('Pass'):
+							try:
+								ftp_bruteforce_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'panelfinder':
-						try:
-							login_panel_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if bruteforce[0]['panelfinder'].startswith('Pass'):
+							try:
+								login_panel_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'ssh_bruteforce':
-						try:
-							ssh_bruteforce_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if bruteforce[0]['ssh_bruteforce'].startswith('Pass'):
+							try:
+								ssh_bruteforce_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'honeypot_detector':
-						try:
-							honeypot_detector_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if informationG[1]['honeypot_detector'].startswith('Pass'):
+							try:
+								honeypot_detector_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'gmail_bruteforce':
-						try:
-							gmail_bruteforce_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
+						if bruteforce[0]['gmail_bruteforce'].startswith('Pass'):
+							try:
+								gmail_bruteforce_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
 					elif user.split(' ')[1] == 'num_verify':
-						try:
-							num_verify_CONFIG()
-						except KeyboardInterrupt:
-							print()
-							main()
-				except IndexError:
-					show_module()
-					print(bold(info('Select a module.')))
+						if informationG[4]['num_verify'].startswith('Pass'):
+							try:
+								num_verify_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
+					elif user.split(' ')[1] == 'mysql_vuln_scanner':
+						if informationG[5]['mysql_vuln_scanner'].startswith('Pass'):
+							try:
+								mysql_vuln_scanner_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
+					elif user.split(' ')[1] == 'ipwhois_extractor':
+						if informationG[2]['ipwhois_extractor'].startswith('Pass'):
+							try:
+								ipwhois_extractor_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
+					elif user.split(' ')[1] == 'atgworm':
+						if exploitation[0]['atgworm'].startswith('Pass'):
+							try:
+								atgworm_CONFIG()
+							except KeyboardInterrupt:
+								print()
+								main()
+						else:
+							print(bold(bad('Install all modules to use.')))
+					else:
+						print(bold(bad('Module not found.')))
+				except Exception as e:
+					pass
 
 			elif user.startswith('set'):
 				try:
@@ -120,35 +202,36 @@ def main():
 
 					if user.split(' ')[1] == 'shodan_api':
 						api[0]['Shodan'] = user.split(' ')[2]
-						print(bold(info('Shodan API\t' + user.split(' ')[2])))
+						print(bold(info('Shodan API set:\t' + user.split(' ')[2])))
 						with open('core/config.yaml', 'w') as cf:
 							yaml.dump(config, cf)
 						cf.close()
 
 					elif user.split(' ')[1] == 'censys_uid':
 						api[1]['Censys UID'] = user.split(' ')[2]
-						print(bold(info('Censys UID\t' + user.split(' ')[2])))
+						print(bold(info('Censys UID set:\t' + user.split(' ')[2])))
 						with open('core/config.yaml', 'w') as cf:
 							yaml.dump(config, cf)
 						cf.close()
 
 					elif user.split(' ')[1] == 'censys_secret':
 						api[2]['Censys SECRET'] = user.split(' ')[2]
-						print(bold(info('Censys SECRET\t' + user.split(' ')[2])))
+						print(bold(info('Censys SECRET set:\t' + user.split(' ')[2])))
 						with open('core/config.yaml', 'w') as cf:
 							yaml.dump(config, cf)
 						cf.close()
 
 					elif user.split(' ')[1] == 'numverify_api':
 						api[3]['Numverify'] = user.split(' ')[2]
-						print(bold(info('Numverify API\t' + user.split(' ')[2])))
+						print(bold(info('Numverify API set:\t' + user.split(' ')[2])))
 						with open('core/config.yaml', 'w') as cf:
 							yaml.dump(config, cf)
 						cf.close()
+					else:
+						print(bold(bad('Command not found.')))
 
 				except IndexError:
-					print(bold(info('Select what to set\n')))
-					print(bold(info('API KEY\t\tset (shodan_api|censys_uid|censys_secret) API_KEY')))
+					show_SET_OPTIONS()
 				except Exception as e:
 					print(bold(bad('Error: {}'.format(str(e)))))
 					main()
@@ -168,16 +251,16 @@ def main():
 						show_help()
 					elif user.split(' ')[1] == 'apis':
 						show_API()
+					elif user.split(' ')[1] == 'history':
+						show_history()
+					else:
+						print(bold(bad('Option not found.')))
 				except IndexError:
-					print(bold(info('Select what to show.\n')))
-					print(bold(info('Modules\t\tshow modules')))
-					print(bold(info('Author\t\tshow author')))
-					print(bold(info('Banners\t\tshow banners')))
-					print(bold(info('Help\t\tshow help')))
-					print(bold(info('API\t\t\tshow apis')))
+					show_SHOW_OPTIONS()
 
 			elif user == 'exit':
 				print(bold(good('Thanks for using Amaterasu.')))
+
 				sys.exit()
 			elif user == 'cls' or user == 'clear':
 				clear()
@@ -185,10 +268,12 @@ def main():
 				update()
 			elif user == 'author':
 				author()
-			elif user == '?':
+			elif user == '?' or user == 'help':
 				show_help()
+			elif user == 'exec':
+				exec(user.split(''))
 			else:
-				print(bad('Amaterasu could not understand.'))
+				print(bold(bad('Command not found.')))
 	except KeyboardInterrupt:
 		print()
 		print(bad('"exit" to get out.'))
