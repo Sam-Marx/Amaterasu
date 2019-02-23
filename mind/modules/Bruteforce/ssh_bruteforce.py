09e8c0a2f3946f4a97fc9ac847ba9cb22cd0ebf7
Sam <sam-marx@protonmail.com>
2019-02-23 00:04:06 -0300

    Visual changed

--- mind/modules/Bruteforce/ssh_bruteforce.py
+++ mind/modules/Bruteforce/ssh_bruteforce.py
@@ -27,24 +27,43 @@ def ssh_bruteforce_CONFIG():
 					print(bold(bad('Error: option do not exist.')))
 					print(bold(info('Select what to set.\n')))
 					print(bold(info('target\tset target TARGET')))
-					print(bold(info('user\tset user USER')))
+					print(bold(info('user\tset user USER (default: 465)')))
 					print(bold(info('password wordlist\tset passwords PASSWORD_LIST')))
 			except IndexError:
 				print(bold(info('Select what to set.\n')))
 				print(bold(info('target\tset target TARGET')))
-				print(bold(info('user\tset user USER')))
+				print(bold(info('user\tset user USER (default: 465)')))
 				print(bold(info('password wordlist\tset passwords PASSWORD_LIST')))
 		elif user.startswith('show'):
 			try:
 				if user.split(' ')[1] == 'config':
-					print(bold(info('Target:\t\t' + target)))
-					print(bold(info('User:\t\t' + user)))
-					print(bold(info('Password wordlist:\t\t' + passwords)))
+					print()
+					sConfig = {'Target': target,
+					'User': user,
+					'Password wordlist': passwords}
+					print(bold('CONFIG\t\t\tVALUE'))
+					print(bold('------\t\t\t-----'))
+					for a, b in sConfig.items():
+						if len(a) > 15:
+							print(bold(a + '\t' + b))
+						elif len(a) <= 6:
+							print(bold(a + '\t\t\t' + b))
+						else:
+							print(bold(a + '\t\t' + b))
 				elif user.split(' ')[1] == 'options':
-					print(bold(info('Select what to set.\n')))
-					print(bold(info('target\tset target TARGET')))
-					print(bold(info('user\tset user USER')))
-					print(bold(info('password wordlist\tset passwords PASSWORD_LIST')))
+					print()
+					sOptions = {'set target [TARGET]': 'Target',
+					'set user [USER]': 'set user to use for smtp connection',
+					'set passwords [PASSWORDS]': 'set password wordlist'}
+					print(bold('COMMAND\t\t\tDESCRIPTION'))
+					print(bold('-------\t\t\t-----------'))
+					for a, b in sOptions.items():
+						if len(a) > 15:
+							print(bold(a + '\t' + b))
+						elif len(a) <= 6:
+							print(bold(a + '\t\t\t' + b))
+						else:
+							print(bold(a + '\t\t' + b))
 				else:
 					print(bold(bad('Error: option do not exist.')))
 			except IndexError:
@@ -53,9 +72,28 @@ def ssh_bruteforce_CONFIG():
 				print(bold(info('Options\t\tshow options')))
 		elif user.startswith('run'):
 			try:
-				ssh_bruteforce(target, user, passwords)
+				gmail_bruteforce(target, user, passwords)
 			except Exception as e:
 				print(bold(bad('Error: {}'.format(e))))
+		elif user == '?' or user == 'help':
+			sHelp = {'help | ?':'print this help message.',
+			'show (config|options)':'show configuration or options',
+			'set target': 'set target to scan',
+			'set passwords': 'set password file to use',
+			'set user': 'set user to attack',
+			'run':'execute module',
+			'back':'back to menu',
+			'exit':'quit from Amaterasu'}
+			print()
+			print(bold('COMMAND\t\t\tDESCRIPTION'))
+			print(bold('-------\t\t\t-----------'))
+			for a, b in sHelp.items():
+				if len(a) > 15:
+					print(bold(a + '\t' + b))
+				elif len(a) <= 6:
+					print(bold(a + '\t\t\t' + b))
+				else:
+					print(bold(a + '\t\t' + b))
 		elif user == 'back':
 			break
 		elif user == 'exit':
