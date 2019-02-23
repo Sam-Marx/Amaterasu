f416666c9b994c36679a70334313bd5064215e14
Sam <sam-marx@protonmail.com>
2019-02-22 23:11:30 -0300

    Update InformationGatheringImports.py

--- mind/modules/InformationGatheringImports.py
+++ mind/modules/InformationGatheringImports.py
@@ -1,10 +1,12 @@
 #coding: utf-8
 #!/usr/bin/python3
 
+#InformationGathering
 from mind.modules.InformationGathering.num_verifier import *
 from mind.modules.InformationGathering.honeypotDetector import *
 from mind.modules.InformationGathering.spider import *
 from mind.modules.InformationGathering.metadata_extractor import *
-#from mind.modules.InformationGathering.subdomain_extractor import *
 from mind.modules.InformationGathering.whois_extractor import *
+from mind.modules.InformationGathering.ipwhois_extractor import *
 from mind.modules.InformationGathering.email_extractor import *
+from mind.modules.InformationGathering.mysql_vuln_scan import *
